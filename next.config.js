1d77f087beed0472f1e1a8f511d0db660abcab75
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-04-19 14:54:19 +0200

    Adapt to new next-transpile-modules api

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const bsconfig = require('./bsconfig.json');
 const withCSS = require('@zeit/next-css');
-const withTM = require("next-transpile-modules");
+
+const transpileModules = ["bs-platform"].concat(bsconfig["bs-dependencies"]);
+const withTM = require("next-transpile-modules")(transpileModules);
+
 const config = {
   target: 'serverless',
   pageExtensions: ["jsx", "js", "bs.js"],
-  transpileModules: ["bs-platform"].concat(bsconfig["bs-dependencies"])
 };
 
 module.exports = withTM(withCSS(config));

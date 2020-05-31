0591f1f5fd4e4ea0a67de162a4fec89a69c76f75
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-05-31 15:25:34 +0200

    Add back cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
+const plugins ={
+  tailwindcss: {},
+  autoprefixer: {}
+};
+
+if(process.env.NODE_ENV === 'production') {
+  plugins.cssnano = {};
 }
+
+module.exports = {
+  plugins: plugins
+};

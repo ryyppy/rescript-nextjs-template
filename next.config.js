0c0213336896ee46cf0e949987c7a24da2c3193b
Patrick Stapfer <ryyppy@users.noreply.github.com>
2021-05-03 21:39:28 +0200

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 const bsconfig = require('./bsconfig.json');
+const fs = require("fs");
 
-const transpileModules = ["bs-platform"].concat(bsconfig["bs-dependencies"]);
+const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
+
+function patchResDeps() {
+  ['rescript'].concat(bsconfig['bs-dependencies']).forEach((resDep) => {
+    fs.writeFileSync(`./node_modules/${resDep}/index.js`, '');
+    const json = require(`./node_modules/${resDep}/package.json`);
+    json.main = 'index.js';
+    fs.writeFileSync(
+      `./node_modules/${resDep}/package.json`,
+      JSON.stringify(json, null, 2),
+    );
+  });
+}
+patchResDeps(); // update package.json and create empty `index.js` before transpiling
 
 const config = {
   target: "serverless",
@@ -14,11 +28,14 @@ const config = {
     if (!isServer) {
       // We shim fs for things like the blog slugs component
       // where we need fs access in the server-side part
-      config.node = {
+      config.resolve.fallback = {
         fs: 'empty'
       }
     }
     return config
+  },
+  future: {
+    webpack5: true
   }
 };
 

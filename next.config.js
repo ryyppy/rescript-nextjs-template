b6e5756efe437f59cc0b9c32995289017639c474
Patrick Stapfer <ryyppy@users.noreply.github.com>
2021-05-07 18:07:16 +0200

    Refine next.config.js

--- next.config.js
+++ next.config.js
@@ -4,19 +4,27 @@ const fs = require("fs");
 const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 
+// There is an issue where webpack doesn't detect npm packages within node_modules when
+// there is no dedicated package.json "main" entry + index.js file existent.
+// This function will make sure that every ReScript dependency folder is conforming
+// to webpack's resolve mechanism
+//
+// This will eventually be removed at some point, so keep an eye out for updates
+// on our template repository.
 function patchResDeps() {
-  ['rescript'].concat(bsconfig['bs-dependencies']).forEach((resDep) => {
-    fs.writeFileSync(`./node_modules/${resDep}/index.js`, '');
-    const json = require(`./node_modules/${resDep}/package.json`);
-    json.main = 'index.js';
+  ["rescript"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
+    fs.writeFileSync(`./node_modules/${bsDep}/index.js`, "");
+    const json = require(`./node_modules/${bsDep}/package.json`);
+    json.main = "index.js";
     fs.writeFileSync(
-      `./node_modules/${resDep}/package.json`,
-      JSON.stringify(json, null, 2),
+      `./node_modules/${bsDep}/package.json`,
+      JSON.stringify(json, null, 2)
     );
   });
 }
 patchResDeps(); // update package.json and create empty `index.js` before transpiling
 
+const isWebpack5 = true;
 const config = {
   target: "serverless",
   pageExtensions: ["jsx", "js"],
@@ -25,17 +33,33 @@ const config = {
   },
   webpack: (config, options) => {
     const { isServer } = options;
-    if (!isServer) {
-      // We shim fs for things like the blog slugs component
-      // where we need fs access in the server-side part
-      config.resolve.fallback = {
-        fs: 'empty'
+
+    if (isWebpack5) {
+      if (!isServer) {
+        // We shim fs for things like the blog slugs component
+        // where we need fs access in the server-side part
+        config.resolve.fallback = {
+          fs: false,
+          path: false,
+        };
       }
+
+      // We need this additional rule to make sure that mjs files are
+      // correctly detected within our src/ folder
+      config.module.rules.push({
+        test: /\.m?js$/,
+        use: options.defaultLoaders.babel,
+        exclude: /node_modules/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        }
+      });
     }
     return config
   },
   future: {
-    webpack5: true
+    webpack5: isWebpack5
   }
 };
 

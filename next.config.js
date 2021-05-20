3951c88a08b2b2952810468010c3e3881d21f3f1
kanishka-work <1385802+kanishka-work@users.noreply.github.com>
2021-05-19 21:49:47 -0400

    Upgrade next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -4,26 +4,6 @@ const fs = require("fs");
 const transpileModules = ["rescript"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 
-// There is an issue where webpack doesn't detect npm packages within node_modules when
-// there is no dedicated package.json "main" entry + index.js file existent.
-// This function will make sure that every ReScript dependency folder is conforming
-// to webpack's resolve mechanism
-//
-// This will eventually be removed at some point, so keep an eye out for updates
-// on our template repository.
-function patchResDeps() {
-  ["rescript"].concat(bsconfig["bs-dependencies"]).forEach((bsDep) => {
-    fs.writeFileSync(`./node_modules/${bsDep}/index.js`, "");
-    const json = require(`./node_modules/${bsDep}/package.json`);
-    json.main = "index.js";
-    fs.writeFileSync(
-      `./node_modules/${bsDep}/package.json`,
-      JSON.stringify(json, null, 2)
-    );
-  });
-}
-patchResDeps(); // update package.json and create empty `index.js` before transpiling
-
 const isWebpack5 = true;
 const config = {
   target: "serverless",
